84b83e88a052ae756f56aef36ad3559a193cbef3
Vinicius Santana <contactuvinicius@gmail.com>
2022-09-14 12:12:28 -0300

    v2

---
+++ script/index.js
@@ -0,0 +1,26 @@
+
+var janela = window.document.getElementsByTagName('main')[0]
+
+// fechar ou minizar a janela
+var close = window.document.getElementById('close')
+close.addEventListener('click', clicar)
+
+var minimize = window.document.getElementById('minimize')
+minimize.addEventListener('click', clicar)
+
+function clicar() {
+
+    janela.style.display = 'none'
+
+}
+// maximizar a janela
+var maximize = window.document.getElementById('maximize')
+maximize.addEventListener('click', maximizar)
+
+function maximizar() {
+
+    janela.style.width = '100%'
+    janela.style.maxWidth = 'none'
+
+}
+
